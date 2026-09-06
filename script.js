1738e3a56623286b45f85ee2ca4d79e8033dca8c
Divjason <sejinpark@tcocs.com>
2026-09-07 08:25:13 +0900

    260907_02

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ============================================
 
 const GOOGLE_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbzmigogD1nqE4uwag0pI3CF4VgV4o7G362LrwJCXZBsKAOd_5oPm4WYv9A1hQHS_eWM/exec";
+  "https://script.google.com/macros/s/AKfycbxOrA7xA3wYJPr_M4ZsaX3s4aYFMQGf30V6392M-ekgt3HecV7oSorl5r08ClgS174k/exec";
 
 // ============================================
 // CURRENT TEST
